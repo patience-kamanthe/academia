1c8529398e4c9dba94f9ded67fc7ce5a5e80e71b
patience <101176716+patience-kamanthe@users.noreply.github.com>
2023-07-08 14:10:09 +0300

    first admin tryout
    the initial admin folder we tried to create

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -75,8 +75,23 @@
             </ul>
           </li>
           <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
-          <li><a class="getstarted scrollto" href="{{ route('register') }}">Register</a></li>
-          <li><a class="getstarted scrollto" href="{{ route('login') }}">Login</a></li>
+          <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
+    @if (Route::has('login'))
+        <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
+            @auth
+                <a href="{{ url('/users/admin') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</a>
+            @else
+                <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log in</a>
+
+                @if (Route::has('register'))
+                    <a href="{{ route('register') }}" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</a>
+                @endif
+            @endauth
+        </div>
+    @endif
+</div>
+          <!-- <li><a class="getstarted scrollto" href="{{ route('register') }}">Register</a></li>
+          <li><a class="getstarted scrollto" href="{{ route('login') }}">Login</a></li> -->
         </ul>
         <i class="bi bi-list mobile-nav-toggle"></i>
       </nav><!-- .navbar -->
--- routes/web.php
+++ routes/web.php
@@ -14,24 +14,33 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-//Route::get('/', function () {
-    //return view('welcome');
-//});
+//  Route::get('/', function () {
+//     return view('welcome');
+//  });
+
 Route::get('/', function () {
     return view('home');
-});
+ });
 
 
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
+});
+
+
+
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::view('/users/admin', 'users.admin')->name('admin');
+    
+    Route::view('/dashboard', 'dashboard')->name('dashboard');
 });
 
 require __DIR__.'/auth.php';
